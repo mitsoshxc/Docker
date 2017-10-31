63f2b7b6320c5829c64ad58ca42fd0dbbcfd170d
Makis Tzakis <gertzakis>
2017-10-31 20:39:34 +0200

    Initial commit of start-all.sh, script for starting all conteiners sequencialy

---
+++ start-all.sh
@@ -0,0 +1,16 @@
+
+# Starting MySql-Server or MSSQL
+# Comment out the prefered one
+./MySql/start
+# ./SqlServer/start
+
+# Starting Nginx Server
+./Nginx/start
+
+# Starting WordPress
+./Wordpress/start
+
+# Starting PhpMyAdmin
+./PhpMyAdmin/start
+
+
