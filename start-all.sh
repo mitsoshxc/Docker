276fd005719e350c6777bce6fc261e7129d32b96
Makis Tzakis <gertzakis>
2017-10-31 20:47:12 +0200

    Added Console Outputs

--- start-all.sh
+++ start-all.sh
@@ -1,16 +1,20 @@
 
 # Starting MySql-Server or MSSQL
 # Comment out the prefered one
+printf "\nStarting SQL container\n\n"
 ./MySql/start
 # ./SqlServer/start
 
 # Starting Nginx Server
+printf "\nStarting Nginx container\n\n"
 ./Nginx/start
 
 # Starting WordPress
+printf "\nStarting wordpress container\n\n"
 ./Wordpress/start
 
 # Starting PhpMyAdmin
+printf "\nStarting phpmyadmin container\n\n"
 ./PhpMyAdmin/start
 
 
