1dea865400fa455b6d3f612d7c3ff2ab1b89237f
mitsoshxc <mitsoshxc@gmail.com>
2017-11-01 09:59:34 +0200

    Changed docker start order from start-all.sh

--- start-all.sh
+++ start-all.sh
@@ -1,20 +1,18 @@
-
 # Starting MySql-Server or MSSQL
 # Comment out the prefered one
-printf "\nStarting SQL container\n\n"
+printf "\nStarting SQL container\n"
 ./MySql/start
 # ./SqlServer/start
 
-# Starting Nginx Server
-printf "\nStarting Nginx container\n\n"
-./Nginx/start
-
 # Starting WordPress
-printf "\nStarting wordpress container\n\n"
+printf "\nStarting wordpress container\n"
 ./Wordpress/start
 
-# Starting PhpMyAdmin
-printf "\nStarting phpmyadmin container\n\n"
-./PhpMyAdmin/start
+# Starting Nginx Server
+printf "\nStarting Nginx container\n"
+./Nginx/start
 
+# Starting PhpMyAdmin
+printf "\nStarting phpmyadmin container\n"
+./PhpMyAdmin/start
 
